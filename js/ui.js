c5f0669800573f8967fcfef937db9c40d9bd67ea
H-hico <55445192+H-hico@users.noreply.github.com>
2019-11-21 08:20:29 -0300

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -5,4 +5,30 @@ const tecnicos = document.querySelector('.tecnicos');
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {edge:'left'});;
+    // formulario para adicionar Area
+	const forms = document.querySelectorAll('.side-form');
+	var instances2 = M.Sidenav.init(forms, {edge:'right'});
   });
+
+//funcao para desenhar o card da receita na tela
+//id, nome, descricao, endereco_imagem
+const desenhaCard = (data, id) => {
+
+    const html = `
+  <div class="col s12 m6 l3 area " data-id="${id}">
+	    <div class="card" style="background-color: #A59C94FF;">
+		    <div class="card-image area-imagem">
+	            <img src="images/${data.endereco_imagem}">
+	            <span class="card-title area-nome">"${data.nome}"</span>
+	        </div>
+	        <div class="card-content area-descricao">
+	            <p>"${data.descricao}"</p>
+	        </div>
+	        <div class="area-deletar">
+                <i class="material-icons" data-id="${id}">delete_outline</i>
+            </div>
+	    </div>
+   </div>`;
+    sobremesas.innerHTML += html;
+
+};
