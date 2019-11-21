398d8337124db3f46542e81e5f528f598d9d8ac5
H-hico <55445192+H-hico@users.noreply.github.com>
2019-11-21 08:04:50 -0300

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,4 +1,8 @@
- document.addEventListener('DOMContentLoaded', function() {
+// essa funcao seleciona o row (que esta no index)
+// para depois desenhar todos os cards la
+const tecnicos = document.querySelector('.tecnicos');
+
+document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
   });
